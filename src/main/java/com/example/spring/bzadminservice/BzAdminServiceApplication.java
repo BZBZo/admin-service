166cbbf59bd492b2d06bdb001da357a7ec86e175
Ninky0 <2020113392@dgu.ac.kr>
2024-12-11 14:11:25 +0900

    init : setting

---
+++ src/main/java/com/example/spring/bzadminservice/BzAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.bzadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BzAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BzAdminServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring/bzadminservice/BzAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.bzadminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BzAdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
